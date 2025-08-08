6d11fa152d80a21b91a5d1b46dd337b22ca7f85c
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-08 22:16:12 +0530

    initials

---
+++ Basics/initials.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+  std::cout<<" ssS  L\n";
+  std::cout<<"S   S L\n";
+  std::cout<<"S     L\n";
+  std::cout<<" S  L\n";
+  std::cout<<"   S  L\n";
+  std::cout<<"S   S L\n";
+  std::cout<<" S  LLLLL\n";
+  
+  return 0;
+}
---
+++ Basics/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+  std::cout<<" sss  l\n";
+  std::cout<<"s   s l\n";
+  std::cout<<"s     l\n";
+  std::cout<<" sss  l\n";
+  std::cout<<"   s  l\n";
+  std::cout<<"s   s l\n";
+  std::cout<<" sss  lllll\n";
+  
+  return 0;
