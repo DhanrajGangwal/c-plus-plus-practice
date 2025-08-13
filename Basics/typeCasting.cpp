ac47abc20afdf724d3f95a34c63c5526de90c26c
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-13 22:58:19 +0530

    typecasting

--- Basics/typeCasting.cpp
+++ Basics/typeCasting.cpp
@@ -5,5 +5,13 @@ int main(){
     float a = 24.55f;
     cout<<"the value of a is "<<a<<endl;
     cout<<"the changed value float to int: "<<int(a)<<endl;
+
+    // ************************ Questions **************************
+    int x = 45;
+    float y = 45.67;
+    cout<<"the expression is x + b: "<<x+y<<endl;
+    cout<<"the expression is x+int(y): "<<x+int(y)<<endl;
+    cout<<"the expression is x+(int)y: "<<x+(int)y<<endl;
+
     return 0;
 }
