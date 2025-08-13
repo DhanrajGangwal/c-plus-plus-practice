1848c1dbaf5c9f2c1d28ebe87675577d3f4ef1ea
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-13 21:46:24 +0530

    build in data types

---
+++ Basics/reference-variables.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int a = 44;
+    int & b = a;
+    cout<<"the value of a is "<<a<<endl;
+    cout<<"the value of b is "<<b<<endl;
+}
---
+++ Basics/typeCasting.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    float a = 24.55f;
+    cout<<"the value of a is "<<a<<endl;
+    cout<<"the changed value float to int: "<<int(a)<<endl;
+    return 0;
+}
