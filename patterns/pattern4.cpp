e518139bda1782da86cacbdf89a96fae85615881
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-14 19:50:44 +0530

    patterns

--- patterns/pattern4.cpp
+++ patterns/pattern4.cpp
@@ -5,12 +5,13 @@ int main(){
     int n;
     cout<<"enter the value of rows: ";
     cin>>n;
-    for (int  i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++)
+    {   
+        char ch = 'A';
         for (int j = 1; j <= i; j++)
         {
-          
-            cout<<i;
+            cout<<ch;
+            ch = ch + 1;
         }
         cout<<"\n";
     }
