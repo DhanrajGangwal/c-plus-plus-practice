d5a061be4244091d91d765a7808247ee5c50e82c
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-18 18:29:49 +0530

    prime or not

--- questions/prime.cpp
+++ questions/prime.cpp
@@ -3,14 +3,27 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"enter the number: ";
+    bool isprime = true;
+    cout<<"enter the number ";
     cin>>n;
-    if (n%2 == 0)       
+    for (int i = 2; i < n; i++)
     {
-       cout<<"not prime";
+        if (n % i == 0)     
+        {
+            isprime = false;
+            break;
+        }
+        
+    }
+
+    if (isprime)
+    {
+        cout<<n<<" is prime number"<<endl;
     }
     else{
-        cout<<"prime";
+        cout<<n<<" is not prime number"<<endl;
     }
+    
+    
     return 0;
 }
---
+++ questions/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+if (n%div==0){
+            cout<<"prime";
+        }
+        else{
+            cout<<"not prime";
+        }
