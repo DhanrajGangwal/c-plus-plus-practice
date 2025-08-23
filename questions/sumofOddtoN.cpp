c772caa354c89c2c1a827b64e862b53bf6bd37ec
Dhanraj Gangwal <dhanrajgangwal01@gmail.com>
2025-08-23 22:22:29 +0530

    sum of odd number to n number

---
+++ questions/sumofOddtoN.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+using namespace std;
+int main(){
+    int n,sum=0,i;
+    cout<<"enter the value: ";
+    cin>>n;
+    for (int i = 1; i <= n; i++)
+    {   
+        if (i%2!=0)
+        {
+            cout<<i<<" ";
+           sum = sum+i;
+        }
+        
+    }
+    
+    cout<<endl<<"the sum of odd nummber 1 to "<<n<<" is "<<sum<<endl;
+    return 0;
+}
